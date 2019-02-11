c92c5f42c6ca1a717b30c9ed3faa7d3e578e13b0
KarolSanttos <karol.pereirainf50@gmail.com>
2019-02-10 21:17:39 -0300

    Questao1

--- estruturas/src/main/java/br/edu/ifce/estruturas/vetor/Vetor.java
+++ estruturas/src/main/java/br/edu/ifce/estruturas/vetor/Vetor.java
@@ -3,27 +3,37 @@ package br.edu.ifce.estruturas.vetor;
 public class Vetor {
 
 	private Aluno[] alunos = new Aluno[100];
-	private int totalDeAlunos = 0;
+	private int totalDeElementos = 0;
 
 	public void adiciona(Aluno aluno) {
-		// implementacao
+	    this.alunos[totalDeElementos] = aluno;
+	    this.totalDeElementos++;
 	}
 
 	public void adiciona(int posicao, Aluno aluno) {
-		// implementacao
+        for(int i = this.totalDeElementos; i>posicao; i--){
+            alunos[i] = alunos[i-1];
+        }
+
+        this.alunos[posicao] = aluno;
+        this.totalDeElementos++;
 	}
 
 	public Aluno pega(int posicao) {
-		// implementacao
-		return null;
+	    if(posicaoOcupada(posicao) == false){
+	        throw new IllegalArgumentException("Posicao invalida");
+	        }
+
+	        return this.alunos[posicao];
 	}
 
-	public void remove(int posicao) {
-		// implementacao
+	public void remover(int posicao) {
+	    this.alunos[totalDeElementos] = aluno;
+	    this.totalDeElementos--;
 	}
 
 	public boolean contem(Aluno aluno) {
-		for (int i = 0; i < this.totalDeAlunos; i++) {
+		for (int i = 0; i < this.totalDeElementos; i++) {
 			if (aluno == this.alunos[i]) {
 				return true;
 			}
@@ -32,23 +42,32 @@ public class Vetor {
 	}
 
 	public int tamanho() {
-		return this.totalDeAlunos;
+		return this.totalDeElementos;
 	}
+	
+    boolean posicaoOcupada(int posicao){
+
+        if(posicao >= 0 && posicao < this.totalDeElementos){
+            return true;
+        } else {
+            return false;
+        }            
+    }
 
 	public String toString() {
-		if (this.totalDeAlunos == 0) {
+		if (this.totalDeElementos == 0) {
 			return "[]";
 		}
 
 		StringBuilder builder = new StringBuilder();
 		builder.append("[");
 
-		for (int i = 0; i < this.totalDeAlunos - 1; i++) {
+		for (int i = 0; i < this.totalDeElementos - 1; i++) {
 			builder.append(this.alunos[i]);
 			builder.append(", ");
 		}
 
-		builder.append(this.alunos[this.totalDeAlunos - 1]);
+		builder.append(this.alunos[this.totalDeElementos - 1]);
 		builder.append("]");
 
 		return builder.toString();
